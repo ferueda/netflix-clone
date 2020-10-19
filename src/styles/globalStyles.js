278f616db9afdef16440373d911873f1a08d7c7d
ferueda <fe.rueda@outlook.com>
2020-10-19 11:58:55 -0300

    Update globalStyles.js
    Removed default img reset

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: inherit;
   }
+
   body, h1, h2, h3, h4, h5, h6, p, ol, ul {
     margin: 0;
     padding: 0;
@@ -16,11 +17,6 @@ const GlobalStyle = createGlobalStyle`
 
   ol, ul {
     list-style: none;
-  }
-
-  img {
-    max-width: 100%;
-    height: auto;
   }
 `;
 
