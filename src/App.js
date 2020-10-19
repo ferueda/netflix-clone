95c4c9ad290794c38abafa94da78f7bc8b2bfbb2
ferueda <fe.rueda@outlook.com>
2020-10-19 11:57:36 -0300

    Add MovieRow components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/globalStyles';
+
+import { requests } from './requests';
+
+import MovieRow from './components/MovieRow';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <div>
+        <Hero />
+        <MovieRow title="Popular on Netflix" request={requests.trendingWeek} />
+        <MovieRow title="Trending Now" request={requests.trendingNow} />
+        <MovieRow title="Top Rated" request={requests.topRated} />
+        <MovieRow isOriginal title="Netflix Originals" request={requests.netflixOriginals} />
+        <MovieRow title="Action Movies" request={requests.actionMovies} />
+        <MovieRow title="Comedy Movies" request={requests.comedyMovies} />
+        <MovieRow title="Romance Movies" request={requests.romanceMovies} />
+        <MovieRow title="Horror Movies" request={requests.horrorMovies} />
+        <MovieRow title="Documentaries" request={requests.documentaries} />
+      </div>
+    </React.Fragment>
   );
 }
 
